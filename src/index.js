9b5842e116ce3aa0c529c568d55658abea969f44
Roman Holubkov <sergeykatelyna@gmail.com>
2020-09-03 23:07:05 +0300

    First implementation of homework #12

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import "./style.scss";
+import './style.scss';
 
-const todoInput = document.querySelector("#todoInput");
-const todoAddBtn = document.querySelector("#todoAddBtn");
-const todoList = document.querySelector(".todoList");
-const todoClearBtn = document.querySelector("#todoClearBtn");
+const todoInput = document.querySelector('#todoInput');
+const todoAddBtn = document.querySelector('#todoAddBtn');
+const todoList = document.querySelector('.todoList');
+const todoClearBtn = document.querySelector('#todoClearBtn');
+
 
 function addtodoLi() {
-  const todoLi = document.createElement("LI");
+  const todoLi = document.createElement('LI');
   todoLi.innerHTML = `
   <span>${todoInput.value}</span>
   <button type="button" class="todoRemoveBtn">&times;</button>
   `;
   todoList.append(todoLi);
-  todoInput.value = "";
+  todoInput.value = '';
 }
 
-todoAddBtn.addEventListener("click", (e) => {
+
+todoAddBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   if (!todoInput.value) {
@@ -25,8 +27,9 @@ todoAddBtn.addEventListener("click", (e) => {
   addtodoLi();
 });
 
-todoInput.addEventListener("keydown", (e) => {
-  if (e.code === "Enter") {
+
+todoInput.addEventListener('keydown', (e) => {
+  if (e.code === 'Enter') {
     e.preventDefault();
 
     if (!todoInput.value) {
@@ -37,21 +40,23 @@ todoInput.addEventListener("keydown", (e) => {
   }
 });
 
-todoList.addEventListener("click", (e) => {
+
+todoList.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (!e.target.matches(".todoRemoveBtn")) {
+  if (!e.target.matches('.todoRemoveBtn')) {
     return;
   }
 
-  const liToRemove = e.target.closest(".todoList > li");
+  const liToRemove = e.target.closest('.todoList > li');
   liToRemove.remove();
 });
 
-todoClearBtn.addEventListener("click", (e) => {
+
+todoClearBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
-  const allTodoLi = document.querySelectorAll(".todoList > li");
+  const allTodoLi = document.querySelectorAll('.todoList > li');
   allTodoLi.forEach((li) => li.remove());
-  todoInput.value = "";
+  todoInput.value = '';
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 
-  devtool: "source-map",
+  devtool: 'source-map',
 
   optimization: {
     minimizer: [new OptimizeCSSAssetsPlugin({})],
@@ -19,36 +19,36 @@ module.exports = {
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
+              name: '[name].[ext]',
             },
           },
         ],
@@ -58,14 +58,14 @@ module.exports = {
 
   devServer: {
     overlay: true,
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     open: true,
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src/index.html"),
+      template: path.join(__dirname, 'src/index.html'),
       minify: {
         collapseWhitespace: true,
         removeComments: true,
